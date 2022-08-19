11e889b83e7949d2dc1dedf9ab9b5c2991e8bc41
Keita0920 <cy20255@shibaura-it.ac.jp>
2022-08-20 03:35:20 +0900

    second git commit -m second

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -10,6 +10,8 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet var drumButton: UIButton!
+    
     let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
 
     override func viewDidLoad() {
@@ -17,10 +19,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func tapDrumButton(){
+    @IBAction func touchDownDrumButton(){
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+        
         drumSoundPlayer.currentTime = 0
         
         drumSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpDrumButton(){
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
     }
 
 
