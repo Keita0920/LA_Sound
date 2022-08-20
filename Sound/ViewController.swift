9f4334900f6f499dbec89e1e2628ad83d88baf80
Keita0920 <cy20255@shibaura-it.ac.jp>
2022-08-20 14:51:53 +0900

    fainal

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -12,7 +12,16 @@ class ViewController: UIViewController {
     
     @IBOutlet var drumButton: UIButton!
     
+    @IBOutlet var pianoButton: UIButton!
+    
+    @IBOutlet var guitarButton: UIButton!
+    
     let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    
+    let pianoSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    
+    let guitarSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +39,30 @@ class ViewController: UIViewController {
     @IBAction func touchUpDrumButton(){
         drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
     }
-
+    
+    @IBAction func touchDownPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        
+        pianoSoundPlayer.currentTime = 0
+        
+        pianoSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    
+    @IBAction func touchDownGuitarButton(){
+        guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+        
+        guitarSoundPlayer.currentTime = 0
+        
+        guitarSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpGuitarButton(){
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
+    }
 
 }
 
